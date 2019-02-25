99cc58f3f08c156fb981cdc02ebec3d218f9ff3f
Jon Sung <jonsung89@gmail.com>
2019-02-24 21:39:23 -0600

    complete xylophone app

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -1,29 +1,36 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 27/01/2016.
-//  Copyright © 2016 London App Brewery. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
+    
+    var player: AVAudioPlayer?
+    let audioList = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
 
     @IBAction func notePressed(_ sender: UIButton) {
         
-        print(sender.tag)
+        playSound(ofThisFileName: audioList[sender.tag - 1])
         
     }
     
+    func playSound(ofThisFileName : String) {
+        
+        let url = Bundle.main.url(forResource: ofThisFileName, withExtension: "wav")!
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            guard let player = player else { return }
+            
+            player.prepareToPlay()
+            player.play()
+        }
+        catch {
+            print(error)
+        }
+    }
   
-
 }
 
